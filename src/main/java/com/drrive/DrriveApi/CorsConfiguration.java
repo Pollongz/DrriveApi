940767a0d8793f0cc72cae209f8c17f619a8156f
Dariusz Bożejewicz <dariusz.bozejewicz@gmail.com>
2021-04-19 21:27:51 +0200

    allowed cors methods

--- src/main/java/com/drrive/DrriveApi/CorsConfiguration.java
+++ src/main/java/com/drrive/DrriveApi/CorsConfiguration.java
@@ -13,7 +13,9 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3000");
             }
         };
     }
