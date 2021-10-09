4fda6ee5ef2acdfc7405142dd2d6dd279201d620
Dariusz Bożejewicz <dariusz.bozejewicz@gmail.com>
2021-10-09 12:15:02 +0200

    added random key generator for tokens and made test for it

---
+++ src/main/java/com/drrive/DrriveApi/security/RandomKey.java
@@ -0,0 +1,2 @@
+package com.drrive.DrriveApi.security;public class RandomKey {
+}
---
+++ src/test/java/com/drrive/DrriveApi/security/RandomKeyTest.java
@@ -0,0 +1,6 @@
+package com.drrive.DrriveApi.security;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RandomKeyTest {
+  
+}
